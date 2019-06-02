e6572c9ef3692b9eebc041147fde8c744ebe3b9d
Olecksandr Gonorovsky <mgonorovpro@gmail.com>
2019-06-02 21:00:38 +0300

    Initial Commit

---
+++ LocalNote100/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNote100
+//
+//  Created by Oleksandr Gonorovskyy on 02/06/2019.
+//  Copyright © 2019 Oleksandr Gonorovskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
